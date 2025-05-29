03d9937f06ba3d09f5dddc84b5f4cfe384445ccf
xuannincntt <xuannincntt@gmail.com>
2025-05-30 00:10:15 +0700

    page home + change language

--- sun_asia/settings.py
+++ sun_asia/settings.py
@@ -47,9 +47,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
             ],
         },
     },
@@ -112,9 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'vi'
 
 LANGUAGES = [
-    ('vi', 'Vietnamese'),
+    ('vi', 'Tiếng Việt'),
     ('en', 'English'),
-    ('jp', 'Japanese'),
+    ('ja', '日本語'),
 ]
 
 TIME_ZONE = 'UTC'
