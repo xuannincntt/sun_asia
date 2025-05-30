bdbf3cdb4a6dd9ce8ffbb39bb7ecce4ba9c78231
Nhan Phat <162662809+leduytan0706@users.noreply.github.com>
2025-05-30 09:00:37 +0700

    product list almost finished

--- products/models.py
+++ products/models.py
@@ -2,22 +2,30 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
 
 # Create your models here.
 class Product(models.Model):
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     desc_file = models.FileField(upload_to='descs/', null=True, blank=True)
     description = models.TextField()
+    stock = models.IntegerField()
     sale_price = models.FloatField()
     org_price = models.FloatField()
     slug = models.SlugField(max_length=255, unique=True)
     image_main = models.ImageField(upload_to='products/')
     image_sub_1 = models.ImageField(upload_to='products/', null=True, blank=True)
     image_sub_2 = models.ImageField(upload_to='products/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+class Categorization(models.Model):
+    category_id = models.ForeignKey('Category', on_delete=models.CASCADE)
+    product_id = models.ForeignKey('Product', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+# from .models import Product
+from django.shortcuts import get_object_or_404
+from django.utils.timezone import now
+
 
 # Create your views here.
-def home(request):
-    return render(request, '/sun_asia/sun_asia/templates/home.html')
+def product_list(request):
+    return render(request, 'product/product_list.html', {'timestamp': now().timestamp()})
+
+def product_detail(request, slug):
+    # product = get_object_or_404(Product, slug=slug)
+    return render(request, 'product/product_detail.html')
