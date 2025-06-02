358dd40fee7b3d1925b97a492a8a2e1c9e23f1c7
Nhan Phat <162662809+leduytan0706@users.noreply.github.com>
2025-06-02 23:08:08 +0700

    Add cart.css and cart.js

---
+++ static/js/cart.js
@@ -0,0 +1,17 @@
+const decreaseBtns = document.querySelectorAll(".minus-icon");
+const increaseBtns = document.querySelectorAll(".plus-icon");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+});
+
+const setQuantityBtnOnClick = () => {
+    increaseBtns.forEach((btn, index) => {
+        
+    });
+
+    decreaseBtns.forEach((btn, index) => {
+
+    });
+};
